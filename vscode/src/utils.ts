c4b76984c7c4529a0314b0d8348a93296c6a42c2
ByFish <maringertheo@gmail.com>
2024-03-23 13:15:37 +0100

    feat(vscode): refacto environment repository

---
+++ vscode/src/utils.ts
@@ -0,0 +1,11 @@
+function getNonce(): string {
+  let nonce: string = "";
+  const possible: string =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < 32; i++) {
+    nonce += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return nonce;
+}
+
+export { getNonce };
