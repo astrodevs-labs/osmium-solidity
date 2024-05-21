77bf5b383b90ccdb0035d4aa54d43025c35a1237
Enzo Bonato <enzo.bonato@epitech.eu>
2024-05-21 14:33:43 -0400

    feat: Fix auto save bug in settings

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -50,16 +50,98 @@ export async function activate(context: ExtensionContext) {
 async function launchFeatures() {
   const configuration = workspace.getConfiguration('Osmium');
 
-  const isLinterEnable = configuration.get('linter');
-  const isSlitherEnable = configuration.get('slither');
-  const isGasEstimationEnable = configuration.get('gas estimation');
-  const isSidebarEnable = configuration.get('sidebar');
-  const isDebuggerEnable = configuration.get('debugger');
-  const isTestsEnable = configuration.get('tests');
-  const isCompilerEnable = configuration.get('compiler');
-  const isreferencesEnable = configuration.get('references');
-  const isAutoFormatEnable = configuration.get('auto format');
-  const isFormatterEnable = configuration.get('formatter');
+	const isLinterEnable = configuration.get('linter');
+	const isSlitherEnable = configuration.get('slither');
+	const isGasEstimationEnable = configuration.get('gas estimation');
+	const isSidebarEnable = configuration.get('sidebar');
+	const isDebuggerEnable = configuration.get('debugger');
+	const isTestsEnable = configuration.get('tests');
+	const isCompilerEnable = configuration.get('compiler');
+	const isreferencesEnable = configuration.get('references');
+	const isAutoFormatEnable = configuration.get('auto format');
+	const isFormatterEnable = configuration.get('formatter');
+	const sidebarProvider = new SidebarProvider(Extcontext.extensionUri);
+	
+	if (isAutoFormatEnable && isFormatterEnable && !saveHandler) {
+		saveHandler = workspace.onDidSaveTextDocument(format);
+	} else if (!isAutoFormatEnable && saveHandler) {
+		saveHandler.dispose();
+		saveHandler = null;
+	}
+	
+	if (isFormatterEnable &&!formatterHandlers) {
+		formatterHandlers = registerForgeFmtLinter(Extcontext);
+	} else if (!isFormatterEnable && formatterHandlers) {
+		formatterHandlers?.fileDisposable.dispose();
+		formatterHandlers?.workspaceDisposable.dispose();
+		formatterHandlers?.formatterDisposable.dispose();
+		formatterHandlers = null;
+	}
+	
+	if (isSidebarEnable && !interactDeployHandler ) {
+		commands.executeCommand('setContext', 'Osmium.showsidebar', true);
+		interactDeployHandler = window.registerWebviewViewProvider(SidebarProvider.viewType, sidebarProvider);
+		Extcontext.subscriptions.push(interactDeployHandler);
+	} else if (!isSidebarEnable && interactDeployHandler) {
+		commands.executeCommand('setContext', 'Osmium.showsidebar', false);
+		interactDeployHandler.dispose();
+		interactDeployHandler = null;
+	}
+	
+	if (isGasEstimationEnable && !gasEstimationHandler) {
+		gasEstimationHandler = registerGasEstimation(Extcontext);
+	} else if (!isGasEstimationEnable && gasEstimationHandler) {
+		gasEstimationHandler.SaveDisposable.dispose();
+		gasEstimationHandler.openDisposable.dispose();
+		gasEstimationHandler.visibleTextEditorsDisposable.dispose();
+		gasEstimationHandler.activeTextEditorDisposable.dispose();
+		gasEstimationHandler.commandDisposable.dispose();
+		gasEstimationHandler = null;
+	}
+	
+	if (isCompilerEnable && !foundryCompilerClient) {
+		foundryCompilerClient = createFoundryCompilerClient(Extcontext);
+		Extcontext.subscriptions.push(foundryCompilerClient);
+	} else if (!isCompilerEnable && foundryCompilerClient) {
+		foundryCompilerClient.stop();
+		foundryCompilerClient = null;
+	}
+	
+	if (isLinterEnable && !linterClient) {
+		linterClient = await createLinterClient(Extcontext);
+		Extcontext.subscriptions.push(linterClient);
+	} else if (!isLinterEnable && linterClient) {
+		linterClient.stop();
+		linterClient = null;
+	}
+	
+	if (isreferencesEnable && !codeActionsClient) {
+		codeActionsClient = await createCodeActionsClient(Extcontext);
+		Extcontext.subscriptions.push(codeActionsClient);
+	} else if (!isreferencesEnable && codeActionsClient) {
+		codeActionsClient.stop();
+		codeActionsClient = null;
+	}
+	
+	if (isSlitherEnable && !slitherClient) {
+		slitherClient = await createSlitherClient(Extcontext);
+		Extcontext.subscriptions.push(slitherClient);
+	} else if (!isSlitherEnable && slitherClient) {
+		slitherClient.stop();
+		slitherClient = null;
+	}
+	
+	if (isDebuggerEnable) {
+	}
+	
+	if (workspace.workspaceFolders?.length && isTestsEnable && !testsPositionsClient) {
+		testsPositionsClient = await createTestsPositionsClient(Extcontext);
+		testManager = new TestManager(testsPositionsClient, workspace.workspaceFolders[0].uri.fsPath);
+		Extcontext.subscriptions.push(testManager.testController, testsPositionsClient);
+	} else if (!isTestsEnable && testsPositionsClient) {
+		testsPositionsClient.stop();
+		testsPositionsClient = null;
+	}
 
   const fsPath = vscode.workspace.workspaceFolders?.[0].uri.fsPath || '';
   const interactContractRepository = new InteractContractRepository(fsPath);
