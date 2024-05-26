32793bd8b3ffd0b400b6bdb8ee61040295f009e0
EnergyCube <dev@energycube.fr>
2024-05-26 08:56:07 -0700

    feat(walkthrough/get-started): foldable and closable view

--- sidebar/src/pages/GetStartedPage/GetStartedPage.tsx
+++ sidebar/src/pages/GetStartedPage/GetStartedPage.tsx
@@ -1,21 +1,27 @@
 import './GetStartedPage.css';
 import { VSCodeButton } from '@vscode/webview-ui-toolkit/react';
+import { useState } from 'react';
 
 export const GetStartedPage = () => {
-  return (
+  const [isOpen, setIsOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(true);
+
+  return isVisible && (
     <div className="getStarted-container">
-      <div className="text-container">
-        <h1 className="title">Get Started with Osmium</h1>
-        <p className="subtitle">Discover Osmium Solidity's features</p>
+      <div className="text-container" onClick={() => setIsOpen(!isOpen)}>
+        <div className="close-button" onClick={() => setIsVisible(false)}>X</div>
+        <h1 className="title">
+          <span style={{ cursor: 'pointer' }}>{isOpen ? '▼' : '►'}</span>
+          Get Started with Osmium 🚀
+        </h1>
+        <p className="subtitle">Unfold to discover Osmium Solidity's features</p>
       </div>
-      <div className="button-container">
-        <VSCodeButton className="documentation-button">
-          Documentation
-        </VSCodeButton>
-        <VSCodeButton className="walkthrough-button">
-          Walkthrough
-        </VSCodeButton>
-      </div>
+      {isOpen && (
+        <div className="button-container">
+          <VSCodeButton className="documentation-button">Documentation</VSCodeButton>
+          <VSCodeButton className="walkthrough-button">Walkthrough</VSCodeButton>
+        </div>
+      )}
     </div>
   );
 };
