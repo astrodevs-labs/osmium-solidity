8cea6825916e6af51ba201ed6085e0345303876c
0xSwapFeeder <tristan.gremont@epitech.eu>
2024-07-23 16:52:08 +0200

    fix(libs/foundry-wrapper): added filter to get only the correct build-info file

--- libs/foundry-wrapper/src/output.rs
+++ libs/foundry-wrapper/src/output.rs
@@ -2,9 +2,8 @@ use crate::error::Error;
 use crate::types::FoundryJsonFile;
 use osmium_libs_solidity_path_utils::join_path;
 
-use std::fs::{read_dir, remove_dir_all, DirEntry};
+use std::fs::{remove_dir_all, read_dir};
 use std::io;
-use std::path::PathBuf;
 
 pub fn remove_previous_outputs(base_path: &str) -> Result<(), Error> {
     let build_info_path = format!("{}/out/build-info", base_path);
@@ -22,7 +21,7 @@ pub fn get_files_from_foundry_output(base_path: &str) -> Result<Vec<FoundryJsonF
     let mut files = Vec::new();
 
     // let init_time = SystemTime::now();
-    let output = std::fs::read_to_string(get_last_build_info(base_path)?)?;
+    let output = get_last_build_info(base_path)?;
     let json: serde_json::Value = serde_json::from_str(&output)?;
     for (file, json) in json["output"]["sources"]
         .as_object()
@@ -47,24 +46,14 @@ pub fn get_files_from_foundry_output(base_path: &str) -> Result<Vec<FoundryJsonF
     Ok(files)
 }
 
-fn get_last_build_info(base_path: &str) -> Result<PathBuf, Error> {
+fn get_last_build_info(base_path: &str) -> Result<String, Error> {
     let out = read_dir(base_path.to_string() + "/out/build-info")?;
 
-    let mut entries: Vec<DirEntry> = out.flatten().collect();
-    entries.sort_by(|a, b| sort_latest(a, b).unwrap_or(std::cmp::Ordering::Equal));
-    let last_build_info = entries.first().ok_or(Error::NoBuildInfo)?;
-    Ok(last_build_info.path())
-}
-
-fn sort_latest(a: &DirEntry, b: &DirEntry) -> Result<std::cmp::Ordering, io::Error> {
-    if let Ok(met_a) = a.metadata() {
-        if let Ok(met_b) = b.metadata() {
-            if met_a.created()? > met_b.created()? {
-                return Ok(std::cmp::Ordering::Greater);
-            } else {
-                return Ok(std::cmp::Ordering::Less);
-            }
+    for entry in out.flatten() {
+        let data: String = std::fs::read_to_string(entry.path())?;
+        if data.contains("\"ast\":") { // Made to differentiate between build-info files and other foundry output files in the same directory
+            return Ok(data);
         }
     }
-    Ok(std::cmp::Ordering::Equal)
+    return Err(Error::NoBuildInfo);
 }
